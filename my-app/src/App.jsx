0f3524033f54a5121639ff793f9e46a97fcff8a0
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-03-03 10:22:31 +0530

    feat: implement routing for Home and About components in App

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,9 +7,12 @@ const App = () => {
 
 return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
         <Conditional />
         <Home />
-        <About /> 
     </div>
 );
 }
