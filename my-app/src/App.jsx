4cdbc903150b24739ee1c1e2780cbde5acdbaad6
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-03-03 10:34:13 +0530

    feat: add react-router-dom for routing and update App component structure

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Conditional from './components/conditional.jsx';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
@@ -7,12 +8,13 @@ const App = () => {
 
 return (
     <div>
+      <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/conditional" element={<Conditional />} />
       </Routes>
-        <Conditional />
-        <Home />
+      </Router>
     </div>
 );
 }
