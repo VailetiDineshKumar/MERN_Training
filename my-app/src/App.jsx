ab8b6d076597ff102787ff3f1a3795e5307af26a
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-03-03 10:16:23 +0530

    feat: add Home and About components to App

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
 import Conditional from './components/conditional.jsx';
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
 const App = () => {
 
 return (
     <div>
         <Conditional />
+        <Home />
+        <About /> 
     </div>
 );
 }
